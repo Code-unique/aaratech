e34e89dbf30e4483caefa046658916b6da6257f8
code-unique <uniqueadhikari2534@gmail.com>
2025-04-07 18:28:24 +0545

    -----

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -55,17 +55,15 @@ const Blogs = () => {
         <h1 className="text-3xl font-bold text-center pb-10 mt-2">Working Process</h1>
 
         {/* Scrollable Animated Sections */}
-        <div className="flex flex-col lg:flex-row items-start justify-between py-10 lg:py-20 space-y-12 lg:space-y-0">
+        <div className="flex items-start justify-between py-20">
           {/* Animated Image Component */}
-          <div className="w-full lg:w-[50%] flex justify-center">
-            <AnimatedImage
-              src={workingStep[currentStep].image}
-              alt={workingStep[currentStep].title}
-            />
-          </div>
+          <AnimatedImage
+            src={workingStep[currentStep].image}
+            alt={workingStep[currentStep].title}
+          />
 
           {/* Scroll-triggered Blog Content */}
-          <div className="w-full lg:w-[50%]">
+          <div className="w-[50%]">
             {workingStep.map((step: any, index: number) => (
               <div
                 key={index}
